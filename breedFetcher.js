ba87257306977e7fd069261b02c141292e69b980
Yonis7 <yonisaden7@gmail.com>
2023-01-21 20:46:35 +0000

    Testing cat service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,19 @@
 const request = require("request");
 
-const breedName = process.argv[2];
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        return callback(error);
+      }
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        return callback(`breed: ${breedName} not found`);
+      }
+      return callback(null, data[0].description);
+    }
+  );
+};
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
-  (error, response, body) => {
-    if (error) {
-      return console.log(error);
-    }
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      return console.log(`breed: ${breedName} not found`);
-    }
-    console.log(data[0].description);
-  }
-);
+module.exports = { fetchBreedDescription };
